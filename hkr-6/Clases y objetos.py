790575cf94e3dfb5d60ac18fa0ddfcb01812cad0
Karla ISA <karliz.0935377k@gmail.com>
2017-06-28 22:13:46 -0500

    Add files via upload

---
+++ hkr-6/Clases y objetos.py
@@ -0,0 +1,30 @@
+#Clases y Objetos
+#Escribir la clase Point con los métodos descritos en HackerRank.
+#Karla Ivonne Serrano Arevalo
+from math import sqrt
+from math import sqrt
+class Point():
+	
+	def __init__(self,x,y):
+        	self.x=x
+		self.y=y
+	def reset(self):
+        	self.x = 0
+        	self.y = 0
+	def move(self,x,y):
+        	self.x+= x
+        	self.y+= y
+	def Distancia(self,punto):
+		Dist=sqrt((punto.x-self.x)**2+(punto.y-self.y)**2)
+		return Dist
+
+D=[]
+
+for i in range((int(raw_input())/2)):
+	D.append(Point(*map(int,raw_input().split())).Distancia(Point(*map(int,raw_input().split()))))
+
+for i in D:
+    print i
+
+
+
